cf4365bdefb53a2e7a9f2f4be04e83ee60061c68
GSDobrev23 <GSDobrev23@codingburgas.bg>
2024-11-19 19:57:53 +0200

    My code for menu

---
+++ project.cpp
@@ -0,0 +1,59 @@
+﻿
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	int choice;
+	cout << "0. Quit" << endl << "1. Start a game" << endl << "2. How to play" << endl;
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 0:
+		cout << "Goodbye" << endl;
+		return 0;
+
+	case 1:
+		cout << "Paste code here..." << endl;
+		break;
+
+	case 2:
+		cout << "How to play" << endl;
+		break;
+
+	default:
+		cout << "Incorrect answer. Choose again..." << endl;
+		break;
+	}
+	
+	do
+	{
+		cout << "0. Quit" << endl << "1. Play again" << endl << "2. How to play" << endl;
+		 cin >> choice;
+
+		switch (choice)
+		{
+		case 0:
+			cout << "Goodbye" << endl;
+			return 0;
+
+		case 1:
+			cout << "Paste code here..." << endl;
+			break;
+
+		case 2:
+			cout << "How to play" << endl;
+			break;
+
+		default:
+			cout << "Incorrect answer. Choose again..." << endl;
+			break;
+
+		}
+	}
+	while (choice != 0);
+}
