6f21aa58a17d3be8dfd73ada129088b99b91cabf
GSDobrev23 <GSDobrev23@codingburgas.bg>
2024-11-19 20:09:28 +0200

    Add second version of menu

--- project/menu 2.cpp
+++ project/menu 2.cpp
@@ -9,30 +9,13 @@ int main()
 {
 	int choice;
 	cout << "0. Quit" << endl << "1. Start a game" << endl << "2. How to play" << endl;
-	cin >> choice;
+	
 
-	switch (choice)
-	{
-	case 0:
-		cout << "Goodbye" << endl;
-		return 0;
-
-	case 1:
-		cout << "Paste code here..." << endl;
-		break;
-
-	case 2:
-		cout << "How to play" << endl;
-		break;
-
-	default:
-		cout << "Incorrect answer. Choose again..." << endl;
-		break;
-	}
+	
 	
 	do
 	{
-		cout << "0. Quit" << endl << "1. Play again" << endl << "2. How to play" << endl;
+		
 		 cin >> choice;
 
 		switch (choice)
@@ -54,6 +37,7 @@ int main()
 			break;
 
 		}
+		cout << "0. Quit" << endl << "1. Play again" << endl << "2. How to play" << endl;
 	}
 	while (choice != 0);
 }
